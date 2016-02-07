59776d95542e788de2692b95d4c5564dd3434b12
geoff.givry <geoff.givry@gmail.com>
2016-02-07 09:05:38 -0500

    added details view

--- vfxremote/urls.py
+++ vfxremote/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
-  url(r'^(?P<choreList_id>[0-9]+)/$', views.details, name='details'),
-  url(r'^(?P<choreList_id>[0-9]+)/vfxremote/$', views.chores, name='chores'),
-  url(r'^(?P<choreList_id>[0-9]+)/vfxremote/(?P<chore_id>[0-9]+)/$', views.choreDetails, name='choreDetails')
+  url(r'^(?P<chorelist_id>[0-9]+)/$', views.details, name='details'),
+  url(r'^(?P<chorelist_id>[0-9]+)/vfxremote/$', views.chores, name='chores'),
+  url(r'^(?P<chorelist_id>[0-9]+)/vfxremote/(?P<chore_id>[0-9]+)/$', views.choreDetails, name='choreDetails')
 ]
--- vfxremote/views.py
+++ vfxremote/views.py
@@ -3,23 +3,19 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 from .models import ChoreList
-from django.template import loader, RequestContext
 
 # Create your views here.
 def index(request):
   lists = ChoreList.objects.all()
-  template = loader.get_template('vfxremote/index.html')
-  context = RequestContext(request, {
-      'chorelists' : lists,
-    })
-  return HttpResponse(template.render(context))
+  return render(request, 'vfxremote/index.html', {'chorelists' : lists})
 
-def details(request, choreList_id):
-  return HttpResponse("This is the Chore List #%s." % (choreList_id))
+def details(request, chorelist_id):
+  list = ChoreList.objects.get(pk=chorelist_id)
+  return render(request, 'vfxremote/detail.html', {'chorelist' : list})
 
-def chores(request, choreList_id):
-  return HttpResponse("This is the Chores of the list : #%s." % (choreList_id))
+def chores(request, chorelist_id):
+  return HttpResponse("This is the Chores of the list : #%s." % (chorelist_id))
 
-def choreDetails(request, choreList_id, chore_id):
-  return HttpResponse("This is the Chore %s of the chore list : #%s." % (chore_id, choreList_id))
+def choreDetails(request, chorelist_id, chore_id):
+  return HttpResponse("This is the Chore %s of the chore list : #%s." % (chore_id, chorelist_id))
 
