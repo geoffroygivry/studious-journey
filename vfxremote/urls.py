26933cce58314c37def5637906629f5f7160b9b7
geoff.givry <geoff.givry@gmail.com>
2016-02-15 09:35:05 -0500

    new views and form creations

--- vfxremote/urls.py
+++ vfxremote/urls.py
@@ -4,6 +4,8 @@ from . import views
 
 urlpatterns = [
   url(r'^$', views.index, name='index'),
+  url(r'^newlist/$', views.newlist, name='newlist'),
   url(r'^(?P<chorelist_id>[0-9]+)/$', views.details, name='details'),
-  url(r'^(?P<chorelist_id>[0-9]+)/chores/(?P<chore_id>[0-9]+)/$', views.choreDetails, name='choreDetails')
+  url(r'^(?P<chorelist_id>[0-9]+)/chores/(?P<chore_id>[0-9]+)/$', views.choreDetails, name='choreDetails'),
+  url(r'^(?P<chorelist_id>[0-9]+)/chores/(?P<chore_id>[0-9]+)/update/$', views.updatechore, name='updatechore')
 ]
--- vfxremote/views.py
+++ vfxremote/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, render
 
 
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, HttpResponseRedirect
 from .models import ChoreList, Chore
 
 # Create your views here.
@@ -19,3 +19,19 @@ def choreDetails(request, chorelist_id, chore_id):
   chore = get_object_or_404(Chore, pk=chore_id)
   return render(request, 'vfxremote/choredetail.html', {'chorelist' : list, 'chore' : chore})
 
+def updatechore(request, chorelist_id, chore_id):
+  chore = get_object_or_404(Chore, pk=chore_id)
+  if 'complete' in request.POST:
+    chore.complete = True
+  else:
+    chore.complete = False
+  chore.save()
+  return HttpResponseRedirect('/vfxremote/' + chorelist_id + '/chores/' + chore_id)
+
+def newlist(request):
+  if request.POST:
+    list = ChoreList(name=request.POST['name'], due_date=request.POST['duedate'])
+    list.save()
+    return HttpResponseRedirect('/vfxremote')
+  else:
+    return render(request, 'vfxremote/newlist.html', {})
