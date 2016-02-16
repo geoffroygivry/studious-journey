e5cd636f354b495f3b0a5163ca41f7217c9db937
geoff.givry <geoff.givry@gmail.com>
2016-02-16 10:53:11 -0500

    config db postgres

--- vfxshare/settings.py
+++ vfxshare/settings.py
@@ -120,3 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Update database configuration with $DATABASE_URL.
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
